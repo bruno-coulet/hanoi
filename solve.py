4ee6d05cacfa5f31ac18840c1e3bb7504ee5b4e1
Bruno Coulet <bruno.coulet@laplateforme.io>
2024-07-16 15:51:59 +0200

    [mise ne place du jeux]

--- solve.py
+++ solve.py
@@ -1,23 +1,47 @@
 '''
 Recevoir en entrée une configuration en une chaîne de caractère,
-Par exemple :“8, 3”
-8 disques et 3 bâtonnets.
+Par exemple :“8, 3”  pour 8 disques et 3 tours.
 '''
 # ------------VERSION 1------------------------------
 start="8,3"
 # Assignation du premier caractère à la variable disque
 total_disques = start[0]
-# Assignation du troisième caractère à la variable battonet
-battonet = start[2]
+# Assignation du troisième caractère à la variable tour
+total_tours = start[2]
 
-# -------------VERSION 2------------------------------- 
+# # -------------VERSION 2------------------------------- 
 # total_disques=input("Choisissez un nombre de disque :")
-# battonet=input("Choisissez un nombre de battonet :")
-# start=f"{battonet},{total_disques}"
+# total_tours=input("Choisissez un nombre de tour :")
+# start=f"{total_tours},{total_disques}"
 # print(start)
 
+
+
+
+
+'''
+AU DEPART, IL Y A LES DISQUES, LES TOURS, TOUS LES DISQUES SONT SUR LA TOUR DE GAUCHE 
+'''
 disques = [i+1 for i in range(int(total_disques))]
-print(disques)
+# print(disques)
+
+tours = [0 for i in range(int(total_tours))]
+tours[0]=len(disques)
+
+jeux = [tours[0],tours[1],tours[2]]
+print(jeux)
+
+
+
+'''déplacement'''
+
+# for d in disques:
+tours[0]-=1
+tours[1]+=1
+
+print(jeux)
+
+
 
 '''
 Afficher dans le terminal la liste des coups à jouer :
