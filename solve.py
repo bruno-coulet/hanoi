e54e34c902497ab946df3c1d8f0b51a4974e1a04
Bruno Coulet <bruno.coulet@laplateforme.io>
2024-07-16 17:21:09 +0200

    [tentative de mouvement]

--- solve.py
+++ solve.py
@@ -4,10 +4,8 @@ Par exemple :“8, 3”  pour 8 disques et 3 tours.
 '''
 # ------------VERSION 1------------------------------
 start="8,3"
-# Assignation du premier caractère à la variable disque
-total_disques = start[0]
-# Assignation du troisième caractère à la variable tour
-total_tours = start[2]
+total_disques = int(start[0])
+total_tours = int(start[2])
 
 # # -------------VERSION 2------------------------------- 
 # total_disques=input("Choisissez un nombre de disque :")
@@ -17,27 +15,28 @@ total_tours = start[2]
 
 
 
-
-
 '''
-AU DEPART, IL Y A LES DISQUES, LES TOURS, TOUS LES DISQUES SONT SUR LA TOUR DE GAUCHE 
-'''
-disques = [i+1 for i in range(int(total_disques))]
-# print(disques)
+au départ, il y a les disques et les tours.'''
+disques = [i+1 for i in range(total_disques)]
+print(f"list des disques : {disques}")
+tours = [0 for i in range(total_tours)]
 
-tours = [0 for i in range(int(total_tours))]
+'''tous les disques sont sur la tour de gauche'''
 tours[0]=len(disques)
+print(f"list des tours en début du jeu: {tours}")
+# tours = [[i for i in range(total_disques, 0, -1)], [], []]
+
 
 jeux = [tours[0],tours[1],tours[2]]
-print(jeux)
+# print(jeux)
 
 
 
 '''déplacement'''
 
-# for d in disques:
-tours[0]-=1
-tours[1]+=1
+for d in range(total_disques):
+    tours[0] = (tours[0])-1
+    tours[1] = (tours[1])+1
 
 print(jeux)
 
