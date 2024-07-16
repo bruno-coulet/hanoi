380f836edf9eee2f13b6cedbd66921751008b256
Bruno Coulet <bruno.coulet@laplateforme.io>
2024-07-16 23:19:13 +0200

    [début de déplacement]

--- solve.py
+++ solve.py
@@ -1,44 +1,56 @@
 '''
 Recevoir en entrée une configuration en une chaîne de caractère,
-Par exemple :“8, 3”  pour 8 disques et 3 tours.
+Par exemple :“8, 3”  pour 8 discs et 3 towers.
 '''
 # ------------VERSION 1------------------------------
-start="8,3"
-total_disques = int(start[0])
-total_tours = int(start[2])
+start_string="8,3"
+#  8 discs
+total_discs = int(start_string[0])
+#  3 towers
+total_towers = int(start_string[2])
 
 # # -------------VERSION 2------------------------------- 
-# total_disques=input("Choisissez un nombre de disque :")
-# total_tours=input("Choisissez un nombre de tour :")
-# start=f"{total_tours},{total_disques}"
+# total_discs=input("Choisissez un nombre de disque :")
+# total_towers=input("Choisissez un nombre de tour :")
+# start=f"{total_towers},{total_discs}"
 # print(start)
 
 
 
-'''
-au départ, il y a les disques et les tours.'''
-disques = [i+1 for i in range(total_disques)]
-print(f"list des disques : {disques}")
-tours = [0 for i in range(total_tours)]
-
-'''tous les disques sont sur la tour de gauche'''
-tours[0]=len(disques)
-print(f"list des tours en début du jeu: {tours}")
-# tours = [[i for i in range(total_disques, 0, -1)], [], []]
+'''au départ, il y a les discs et les towers.'''
+discs = [i+1 for i in range(total_discs)]
+print(f"list des discs : {discs}")
+towers = [0 for i in range(total_towers)]
+print(f"list des towers: {towers}")
 
 
-jeux = [tours[0],tours[1],tours[2]]
-# print(jeux)
+
+'''tous les discs sont sur la tour de gauche'''
+towers[0]=len(discs)
+print(f"list des towers en début du jeu: {towers}")
+
+
+
+game = [towers[0],towers[1],towers[2]]
+# print(game)
 
 
 
 '''déplacement'''
+def move(source, destination):
+    if towers[source]>0:
+        towers[source]-=1
+        towers[destination]+=1
 
-for d in range(total_disques):
-    tours[0] = (tours[0])-1
-    tours[1] = (tours[1])+1
+        print(f"Déplacer un disque de la tour {source} à la tour {destination}")
+    else:
+        print(f"Erreur : la tour {source} est vide et ne peut pas déplacer de disque")
 
-print(jeux)
+# Déplacement d'un disque de la tour 1 à la tour 2
+move(0, 1)
+print("Après avoir déplacé le disque de la tour 1 à la tour 2 :")
+print(towers)
+
 
 
 
