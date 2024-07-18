76cf41cd75f5d86ad1e42ae79ab367cc1b871904
Bruno Coulet <bruno.coulet@laplateforme.io>
2024-07-18 23:33:26 +0200

    test de configutation via une chaine

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-from solve import Solver
-from graphics import Graphic
+from solve import solve
+from graphics import *
 
-class MainRun(Solver, Graphic):
+
+disks=int(input("Choisissez un nombre de disque :"))
+towers=int(input("Choisissez un nombre de tour :"))
+start_string=f"{towers},{disks}"
+print(start_string)
+
+disk_list = [i+1 for i in range(disks)]
+# print(f"disk_list : {disk_list}")
+# print(type(disk_list))
+tower_list = [f"tour_{i+1}"  for i in range(towers)]
+
+class MainRun():
     def __init__(self):
-        Solver.__init__(self)
-        Graphic.__init__(self)
+        solve(self)
+        graphic(self)
         self.moves = []
         self.move_index = 0
 
     def running(self):
-        self.solve("1", "3", "2", 8, self.moves)
+        self.solve("1", "3", "2", disks, self.moves)
         self.run()
         
 game_page = MainRun()
--- solve.py
+++ solve.py
@@ -1,6 +1,30 @@
-class Solver():
-    def solve(self, t_1, t_3, t_2, disks, moves):
-        if disks > 0:
-            self.solve(t_1, t_2, t_3, disks - 1, moves)
-            moves.append((t_1, t_3))
-            self.solve(t_2, t_3, t_1, disks - 1, moves)
+from main import disks, towers, disk_list, tower_list
+
+moves = []
+
+print(f"disques : {disks}")
+print(f"tours : {towers}")
+
+def solve(tower_1, tower_3, tower_2, disks):
+    if disks > 0:
+        solve(tower_1, tower_2, tower_3, disks - 1)
+        moves.append((tower_1, tower_3))
+        solve(tower_2, tower_3, tower_1, disks - 1)
+    return tower_1, tower_3, tower_2
+
+
+# Appel de la fonction solve pour résoudre le problème de la Tour de Hanoï
+solve(tower_list[0], tower_list[2], tower_list[1], disks)
+
+print(f"moves : {moves}")
+
+
+
+
+
+# class Solver():
+#     def solve(self, t_1, t_3, t_2, disks, moves):
+#         if disks > 0:
+#             self.solve(t_1, t_2, t_3, disks - 1, moves)
+#             moves.append((t_1, t_3))
+#             self.solve(t_2, t_3, t_1, disks - 1, moves)
