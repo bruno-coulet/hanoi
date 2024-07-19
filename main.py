dbda5cca37d266c689651d1123e0e52830acc0d9
ines-lorquet <ines.lorquet@laplateforme.io>
2024-07-19 09:31:14 +0200

    Black les fichiers

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from solve import Solver
 from graphics import Graphic
 
+
 class MainRun(Solver, Graphic):
     def __init__(self):
         Solver.__init__(self)
@@ -10,10 +11,11 @@ class MainRun(Solver, Graphic):
         self.t_1 = list(range(8, 0, -1))
         self.t_2 = []
         self.t_3 = []
-        
+
     def running(self):
         self.solve("1", "3", "2", 8, self.moves)
         self.run()
-        
+
+
 game_page = MainRun()
 game_page.running()
