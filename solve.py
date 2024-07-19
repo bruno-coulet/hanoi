a140ae415bf01bb7af439b861c1750057558a1d0
ines-lorquet <ines.lorquet@laplateforme.io>
2024-07-19 10:11:52 +0200

    Ajout solve_terminal

--- solve.py
+++ solve.py
@@ -4,3 +4,12 @@ class Solver:
             self.solve(t_1, t_2, t_3, disks - 1, moves)
             moves.append((t_1, t_3))
             self.solve(t_2, t_3, t_1, disks - 1, moves)
+
+    def solve_terminal(self, t_1, t_3, t_2, disks):
+        if disks > 0:
+            self.solve_terminal(t_1, t_2, t_3, disks - 1)
+            print(t_1, "->", t_3)
+            self.solve_terminal(t_2, t_3, t_1, disks - 1)
+
+# solver_terminal = Solver()
+# solver_terminal.solve_terminal("1", "3", "2", 16)
